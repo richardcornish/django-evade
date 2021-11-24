6efa5a935eac9b1472cd74d3b897c09063f962a3
Richard Cornish <richardcornish@gmail.com>
2021-11-23 16:07:24 -0800

    Excluded tests from installed package

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     author_email="rich@richardcornish.com",
     url="https://github.com/richardcornish/django-evade",
     license="BSD",
-    packages=find_packages(),
+    packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     zip_safe=False,
     classifiers=[
