e0f2002ee16d245e092dcaf1b1792acb6db4db85
Richard Cornish <richardcornish@gmail.com>
2022-04-24 00:03:56 -0700

    Added comments

--- evade/evade.py
+++ evade/evade.py
@@ -1,6 +1,8 @@
 import random
 from html.entities import html5
 
+# HTML5 named character references
+# https://docs.python.org/3/library/html.entities.html#html.entities.html5
 HTML5_CHARS = {html5[key]: key for key in html5}
 
 ENTITIES = {
@@ -14,18 +16,24 @@ def evade(value):
     s = ""
     for char in value:
 
-        # Named entities require simpler syntax
+        # HTML5 named character references
         if char in HTML5_CHARS and random.randint(0, 1):
             name = HTML5_CHARS[char]
             s += "&{}".format(name)
         else:
+
+            # Decimal and hexadecimal references
             cp = ord(char)
             choice = random.choice(list(ENTITIES.items()))
             form = choice[1]
             fill = "0"
             width = random.randint(0, 4)
             tipe = choice[0]
+
+            # Format Specification Mini-Language
+            # https://docs.python.org/3/library/string.html#formatspec
             s += "&{form}{:{fill}{width}{type}};".format(
                 cp, form=form, fill=fill, width=width, type=tipe
             )
+
     return s
